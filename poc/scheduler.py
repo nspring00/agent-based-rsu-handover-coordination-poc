bc10c8169076014a5a47e7d91452ec99acec1b44
Nikolaus Spring <nikolaus.spring@gmail.com>
2024-08-01 14:12:20 +0200

    Add comments to scheduler

--- poc/scheduler.py
+++ poc/scheduler.py
@@ -1,17 +1,42 @@
 from typing import Iterable, List, TypeVar, Type
 
-import mesa
 from mesa import Model, Agent
+from mesa.time import RandomActivationByType
 
 T = TypeVar('T', bound=Agent)
 
 
-class RandomActivationBySortedType(mesa.time.RandomActivationByType):
+class RandomActivationBySortedType(RandomActivationByType):
+    """
+    A scheduler which activates each type of agent once per step, in the specified agent order, but all the
+    same type are activated in a random order. This is equivalent to the RandomActivationByType model, but with a fixed
+    order of agent types.
+    """
+
     def __init__(self, model: Model, types: List[type(Agent)], agents: Iterable[Agent] | None = None) -> None:
+        """
+        Create a new RandomActivationBySortedType scheduler.
+
+        Args:
+            model (Model): The model to which the schedule belongs
+            types (List[type(Agent)]): A list of agent types to be executed in the specified order
+            agents (Iterable[Agent], None, optional): An iterable of agents who are controlled by the schedule
+        """
+
         super().__init__(model, agents)
         self.types = types
 
     def step(self, shuffle_types: bool = False, shuffle_agents: bool = True) -> None:
+        """
+        Executes the step of each agent type, one at a time, in the specified order.
+
+        Args:
+            shuffle_types: If True, the order of execution of each types is
+                           shuffled.
+            shuffle_agents: If True, the order of execution of each agents in a
+                            type group is shuffled.
+        """
+
         if shuffle_types:
             self.model.random.shuffle(self.types)
         for agent_type in self.types:
@@ -23,4 +48,8 @@ class RandomActivationBySortedType(mesa.time.RandomActivationByType):
         self.time += 1
 
     def get_agents_by_type(self, agent_type: Type[T]) -> List[T]:
+        """
+        Returns the agents of a given type.
+        """
+
         return self._agents_by_type.get(agent_type, [])
