db455a3b6f8b68ba8c75d1adc1cb6d0f64ce2eec
Nikolaus Spring <54682104+nspring00@users.noreply.github.com>
2024-10-22 22:18:48 +0200

    Add figures for defensio (#1)
    * Simple HO frequency comparison v1
    
    * Simple QoS comparison
    
    * Improve defensio plots
    
    * Improve defensio plots
    
    * Improve defensio plots
    
    * Improve defensio plots
    
    * Improve defensio plots
    
    * Improve defensio plots
    
    * Improve qos plot
    
    * Update x axis labels
    
    * Update qos whiskers

--- utils/visualize_results.py
+++ utils/visualize_results.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
 from datetime import datetime, timedelta
+from enum import IntFlag
+from xml.dom import DOMException
 
 import numpy as np
 import pandas as pd
@@ -7,7 +10,7 @@ import unidecode
 from matplotlib.patches import Rectangle, Circle
 from matplotlib.ticker import PercentFormatter
 
-from poc.capacity_vanet import get_grid
+from poc.VanetTraceLoader import get_grid
 from poc.render import VEC_STATION_COLORS
 from poc.scenarios import CRETEIL_4_RSU_FULL_CAPA_CONFIG, CRETEIL_9_RSU_FULL_CAPA_CONFIG, \
     CRETEIL_3_FAIL_RSU_FULL_CAPA_CONFIG
@@ -303,7 +306,7 @@ def plot_rsu_config(rsu_config: list[RsuConfig], name: str):
     ax.set_yticks([])
     fig.tight_layout()
     filename = f'rsu_config_{name}.png'
-    fig.savefig(filename, format='png', dpi=200)
+    fig.savefig(filename, format='png', dpi=200, transparent=True)
     plt.show()
 
 
@@ -337,6 +340,335 @@ results_creteil_failure = [
     ("results_creteil-morning_3-fail-half", "Morning Half Capacity"),
 ]
 
+results_tradition_vs_base = [
+    ("results_creteil-morning_4-full", "Sparse & Full Capacity"),
+    ("results_creteil-morning_4-half", "Sparse & Half Capacity"),
+    ("results_creteil-morning_9-full", "Dense & Full Capacity"),
+    ("results_creteil-morning_9-half", "Dense & Half Capacity"),
+    ("results_creteil-morning_9-quarter", "Dense & Quarter Capacity"),
+]
+
+
+def plot_total_ho_frequency(configs, title, field):
+    # Prepare data
+    data = []
+    for filename, res_title in configs:
+        df = pd.read_csv(f"../results/{filename}.csv")
+        df = df.sort_values(by='Model', key=lambda x: x.map(custom_sort_key))
+        data.append((filename, df, res_title))
+
+    # Extract total handover frequency data
+    ho_data_2 = []
+
+    arhc = ['ARHC-Oracle', 'ARHC-10s', 'ARHC-20s']
+    traditional = ['EarliestHO', 'LatestHO', 'NearestRSU']
+    done = defaultdict(lambda: False)
+
+    group_names = []
+
+    for filename, df, res_title in data:
+        group_names.append(res_title)
+        ho_data_2.append([df[df['Model'] == model][field].sum() for model in arhc])
+        is_sparse = '4' in filename
+        if is_sparse and done['sparse']:
+            continue
+        if not is_sparse and done['dense']:
+            continue
+
+        done['sparse' if is_sparse else 'dense'] = True
+        # group_names.append("Sparse" if is_sparse else "Dense")
+        ho_data_2.append([df[df['Model'] == model][field].sum() for model in traditional])
+
+    # Swap entry 1 and 2
+    ho_data_2[1], ho_data_2[2] = ho_data_2[2], ho_data_2[1]
+    # Swap entry 4 and 5_2_2_2
+    ho_data_2[4], ho_data_2[5] = ho_data_2[5], ho_data_2[4]
+    # Swap entry 5 and 6_2_2_2
+    ho_data_2[5], ho_data_2[6] = ho_data_2[6], ho_data_2[5]
+
+    ho_data = list(map(list, zip(*ho_data_2)))
+
+    # Determine if a break is needed
+    break_threshold = 20000
+    max_value = max(max(x) for x in ho_data)
+    needs_break = max_value > break_threshold
+
+    # w = 3 * len(configs)
+    w = 12
+    # Plotting
+    if needs_break:
+        fig, (ax, ax2) = plt.subplots(2, 1, sharex=True, figsize=(w, 8),
+                                      gridspec_kw={'height_ratios': [1, 6], 'hspace': 0.07})
+    else:
+        fig, ax2 = plt.subplots(figsize=(w, 8))
+
+    bar_width = 0.2
+    gap_width = 0.2  # Width of the gap between groups
+    margin = 0.03
+    offset = 0.4
+    index = np.array([0, 1, 2, 3 + offset, 4 + offset, 5 + offset, 6 + offset]) * (
+            len(ho_data) * bar_width + gap_width)  # Add space between groups
+
+    colors = plt.get_cmap('Set1', 6)
+
+    for i, counts in enumerate(ho_data):
+        color = [colors(i) for _ in range(5)]
+        color.insert(2, colors(i + 3))
+        color.append(colors(i + 3))
+        if needs_break:
+            ax.bar(index + i * bar_width, counts, bar_width - margin, label="a", color=color, edgecolor='black')
+        ax2.bar(index + i * bar_width, counts, bar_width - margin, label="a", color=color, edgecolor='black')
+
+    if needs_break:
+        # Set the y-axis limits for the break
+        ax.set_ylim(break_threshold, max_value)
+        ax2.set_ylim(0, break_threshold)
+
+        # Hide the spines between ax and ax2
+        ax.spines['bottom'].set_visible(False)
+        ax2.spines['top'].set_visible(False)
+        ax.xaxis.tick_top()
+        ax.tick_params(labeltop=False)  # Don't put tick labels at the top
+        ax2.xaxis.tick_bottom()
+
+        # Add diagonal lines to indicate the break
+        d = .010  # How big to make the diagonal lines in axes coordinates
+        kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
+        ax.plot((-d, +d), (-5 * d, +5 * d), **kwargs)  # Top-left diagonal
+        ax.plot((1 - d, 1 + d), (-5 * d, +5 * d), **kwargs)  # Top-right diagonal
+
+        kwargs.update(transform=ax2.transAxes)  # Switch to the bottom axes
+        ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # Bottom-left diagonal
+        ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # Bottom-right diagonal
+
+    vertical_line_position = 2 + offset - gap_width / 4 + 0.02
+    if needs_break:
+        ax.axvline(x=vertical_line_position, color='gray', linestyle='--')
+    ax2.axvline(x=vertical_line_position, color='gray', linestyle='--')
+
+    ax2.set_xlabel('Configurations', fontsize=14)
+    ax2.set_ylabel('Number of Handovers', fontsize=14)
+    ax2.set_xticks(index + bar_width * (len(ho_data) - 1) / 2)
+    x_ticks = group_names
+    x_ticks.insert(2, "Sparse\n(cap. indep.)")
+    x_ticks.append("Dense\n(cap. indep.)")
+    ax2.set_xticklabels(x_ticks, rotation=0, ha='center', fontsize=12)
+    legend_loc = "lower left" if not needs_break else "upper left"
+    # ax2.legend(title="Handover Coordination Strategy", loc=legend_loc, fontsize=12, title_fontsize=14)
+    legend_colors = [colors(i) for i in range(6)]
+    legend_labels = ['ARHC Oracle', 'ARHC 10s', 'ARHC 20s', 'Earliest HO', 'Latest HO', 'Nearest RSU']
+    ax2.legend(handles=[plt.Line2D([0], [0], color=color, lw=12) for color in legend_colors], labels=legend_labels,
+               title="HO Coordination Strategy", loc=legend_loc, fontsize=12, title_fontsize=14)
+    title_ax = ax if needs_break else ax2
+    title_ax.set_title(
+        f"Number of Handovers per Configuration", fontsize=20)
+    ax2.grid(True, linestyle='--', alpha=0.7)
+    if needs_break:
+        ax.grid(True, linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.savefig(f'{unidecode.unidecode(title).strip().lower().replace(" ", "_")}.png',
+                format="png", dpi=200, transparent=True)
+    plt.show()
+
+
+def plot_boxplot(configs, y_axis, field, title, percentage=False):
+    # Prepare data
+    data = []
+    for filename, res_title in configs:
+        df = pd.read_csv(f"../results/{filename}.csv")
+        df = df.sort_values(by='Model', key=lambda x: x.map(custom_sort_key))
+        data.append((filename, df, res_title))
+
+    # Extract total handover frequency data
+    qos_data = {}
+    for filename, df, res_title in data:
+        for model in df['Model'].unique():
+            if model.startswith('ARHC-') and model not in ['ARHC-Oracle', 'ARHC-10s', 'ARHC-20s']:
+                continue
+            if model not in qos_data:
+                qos_data[model] = []
+            details_file = f"../results/runs/{filename.replace('results', 'result')}_{model.lower()}_model_vars.csv"
+            # Read using pands and extract column MinQos
+            df = pd.read_csv(details_file)
+            qos_values = df[field][1:-1]  # Ignore first and last due to outliers (no vehicles)
+            qos_data[model].append(qos_values)
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    bar_width = 0.2
+    small_gap_width = 0.1  # Smaller gap between the first three values
+    large_gap_width = 0.2  # Width of the gap between groups
+    margin = 0.03
+    offset = 0.2
+    offset_from = 1 if len(configs) == 3 else 2
+    base_arr = np.arange(len(configs))
+    base_arr = base_arr.astype(np.float64)
+    base_arr[offset_from:] += offset
+    index = base_arr * (
+            len(qos_data) * bar_width + small_gap_width + large_gap_width)  # Add space between groups
+
+    colors = plt.get_cmap('Set1', len(qos_data))
+    legend_labels = []
+    legend_colors = []
+
+    avg_color = plt.get_cmap('Set1', 10)(7)
+
+    for i, (model, counts) in enumerate(qos_data.items()):
+        if i >= 3:
+            positions = index + i * bar_width + small_gap_width / 2
+        else:
+            positions = index + i * bar_width - small_gap_width / 2
+        color = colors(i)
+        ax.boxplot(counts, positions=positions, widths=bar_width - margin, patch_artist=True,
+                   boxprops=dict(facecolor=color),
+                   medianprops=dict(color='black'), whiskerprops=dict(color='black'), capprops=dict(color='black'),
+                   flierprops=dict(marker='o', color='gray', alpha=0.4, markersize=2),
+                   whis=[1, 99])  # Set lower whisker to 9th percentile and upper whisker to 91st percentile
+        averages = [np.mean(c) for c in counts]
+        ax.scatter(positions, averages, color=color, edgecolors='black', linewidths=1, s=180, zorder=3, label='Average' if i == 0 else "", marker='D')
+        legend_labels.append(model)
+        legend_colors.append(color)
+
+    vertical_line_position = offset_from + 0.42
+    ax.axvline(x=vertical_line_position, color='gray', linestyle='--')
+
+    ax.set_xlabel('Configurations', fontsize=14)
+    ax.set_ylabel(y_axis, fontsize=14)
+    if percentage:
+        plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x:.0%}'))
+    ax.set_xticks(index + bar_width * (len(qos_data) - 1) / 2)
+    ax.set_xticklabels([res_title for _, _, res_title in data], rotation=0, ha='center', fontsize=12)
+    legend_pos = "lower left" if field == "MinQoS" else "upper left"
+    legend_labels = ['ARHC Oracle', 'ARHC 10s', 'ARHC 20s', 'Earliest HO', 'Latest HO', 'Nearest RSU']
+    ax.legend(handles=[plt.Line2D([0], [0], color=color, lw=12) for color in legend_colors] +
+                      [plt.Line2D([0], [0], color=avg_color, marker='D', lw=0, markersize=10)],
+              labels=legend_labels + ['Average', 'P01'],
+              title="HO Coordination Strategy", loc=legend_pos, fontsize=12, title_fontsize=14)
+    ax.set_title(title, fontsize=20)
+    ax.grid(True, linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.savefig(f'{unidecode.unidecode(y_axis).strip().lower().replace(" ", "_")}.png',
+                format="png", dpi=200, transparent=True)
+    plt.show()
+
+
+def plot_boxplot_gini(configs, y_axis, field, title, percentage=False):
+    # Prepare data
+    data = []
+    for filename, res_title in configs:
+        df = pd.read_csv(f"../results/{filename}.csv")
+        df = df.sort_values(by='Model', key=lambda x: x.map(custom_sort_key))
+        data.append((filename, df, res_title))
+
+    # Extract total handover frequency data
+    qos_data = {}
+    for filename, df, res_title in data:
+        for model in df['Model'].unique():
+            if model.startswith('ARHC-') and model not in ['ARHC-Oracle', 'ARHC-10s', 'ARHC-20s']:
+                continue
+            if model not in qos_data:
+                qos_data[model] = []
+            details_file = f"../results/runs/{filename.replace('results', 'result')}_{model.lower()}_model_vars.csv"
+            # Read using pands and extract column MinQos
+            df = pd.read_csv(details_file)
+            qos_values = df[field][1:-1]  # Ignore first and last due to outliers (no vehicles)
+            qos_data[model].append(qos_values)
+
+    ho_data_2 = []
+
+    arhc = ['ARHC-Oracle', 'ARHC-10s', 'ARHC-20s']
+    traditional = ['EarliestHO', 'LatestHO', 'NearestRSU']
+    done = defaultdict(lambda: False)
+
+    group_names = []
+
+    for filename, df, res_title in data:
+        group_names.append(res_title)
+        ho_data_2.append([])
+        for model in arhc:
+            details_file = f"../results/runs/{filename.replace('results', 'result')}_{model.lower()}_model_vars.csv"
+            df = pd.read_csv(details_file)
+            qos_values = df[field][1:-1]  # Ignore first and last due to outliers (no vehicles)
+            ho_data_2[-1].append(qos_values)
+        is_sparse = '4' in filename
+        if is_sparse and done['sparse']:
+            continue
+        if not is_sparse and done['dense']:
+            continue
+
+        ho_data_2.append([])
+        for model in traditional:
+            details_file = f"../results/runs/{filename.replace('results', 'result')}_{model.lower()}_model_vars.csv"
+            df = pd.read_csv(details_file)
+            qos_values = df[field][1:-1]
+            ho_data_2[-1].append(qos_values)
+
+        done['sparse' if is_sparse else 'dense'] = True
+        # group_names.append("Sparse" if is_sparse else "Dense")
+        # ho_data_2.append([df[df['Model'] == model][field].sum() for model in traditional])
+
+    # Swap entry 1 and 2
+    ho_data_2[1], ho_data_2[2] = ho_data_2[2], ho_data_2[1]
+    # Swap entry 4 and 5_2_2_2
+    ho_data_2[4], ho_data_2[5] = ho_data_2[5], ho_data_2[4]
+    # Swap entry 5 and 6_2_2_2
+    ho_data_2[5], ho_data_2[6] = ho_data_2[6], ho_data_2[5]
+
+    qos_data = list(map(list, zip(*ho_data_2)))
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    bar_width = 0.2
+    small_gap_width = 0.1  # Smaller gap between the first three values
+    large_gap_width = 0.2  # Width of the gap between groups
+    margin = 0.03
+    offset = 0.2
+    offset_from = 3
+    base_arr = np.arange(7)
+    base_arr = base_arr.astype(np.float64)
+    base_arr[offset_from:] += offset
+    index = base_arr * (len(qos_data) * bar_width + large_gap_width)  # Add space between groups
+
+    colors = plt.get_cmap('Set1', 6)
+    legend_colors = []
+
+    for i, counts in enumerate(qos_data):
+        positions = index + (i - 1) * bar_width
+        color = [colors(i) for _ in range(5)]
+        color.insert(2, colors(i + 3))
+        color.append(colors(i + 3))
+        for val, pos, col in zip(counts, positions, color):
+            ax.boxplot([val], positions=[pos], widths=bar_width - margin, patch_artist=True,
+                       boxprops=dict(facecolor=col),
+                       medianprops=dict(color='black'), whiskerprops=dict(color='black'), capprops=dict(color='black'),
+                       flierprops=dict(marker='o', color='gray', alpha=0.4, markersize=2))
+        legend_colors.append(color)
+
+    vertical_line_position = offset_from - 1 + 0.08
+    ax.axvline(x=vertical_line_position, color='gray', linestyle='--')
+
+    ax.set_xlabel('Configurations', fontsize=14)
+    ax.set_ylabel(y_axis, fontsize=14)
+    if percentage:
+        plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x:.0%}'))
+    ax.set_xticks(index)
+    x_labels = [res_title for _, _, res_title in data]
+    x_labels.insert(2, "Sparse\n(cap. indep.)")
+    x_labels.append("Dense\n(cap. indep.)")
+    ax.set_xticklabels(x_labels, rotation=0, ha='center', fontsize=12)
+    legend_colors = [colors(i) for i in range(6)]
+    legend_pos = "lower left" if field == "MinQoS" else "upper left"
+    legend_labels = ['ARHC Oracle', 'ARHC 10s', 'ARHC 20s', 'Earliest HO', 'Latest HO', 'Nearest RSU']
+    ax.legend(handles=[plt.Line2D([0], [0], color=color, lw=12) for color in legend_colors],
+              labels=legend_labels,
+              title="HO Coordination Strategy", loc=legend_pos, fontsize=12, title_fontsize=14)
+    ax.set_title(title, fontsize=20)
+    ax.grid(True, linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.savefig(f'{unidecode.unidecode(y_axis).strip().lower().replace(" ", "_")}.png',
+                format="png", dpi=200, transparent=True)
+    plt.show()
+
 
 # Main function to visualize results
 def main():
@@ -352,9 +684,34 @@ def main():
     # plot_metrics_over_time("creteil-morning", "3-fail-full", "arhc-01s", morning=True)
     # plot_metrics_over_time("creteil-morning", "3-fail-half", "arhc-01s", morning=True)
 
-    plot_rsu_config(CRETEIL_4_RSU_FULL_CAPA_CONFIG, "creteil_4")
-    plot_rsu_config(CRETEIL_9_RSU_FULL_CAPA_CONFIG, "creteil_9")
-    plot_rsu_config(CRETEIL_3_FAIL_RSU_FULL_CAPA_CONFIG, "creteil_3_fail")
+    # plot_rsu_config(CRETEIL_4_RSU_FULL_CAPA_CONFIG, "creteil_4")
+    # plot_rsu_config(CRETEIL_9_RSU_FULL_CAPA_CONFIG, "creteil_9")
+    # plot_rsu_config(CRETEIL_3_FAIL_RSU_FULL_CAPA_CONFIG, "creteil_3_fail")
+
+    cap_suffix = " Cap."
+
+    plot_total_ho_frequency([
+        ("results_creteil-morning_4-full", "Sparse NW\nFull" + cap_suffix),
+        ("results_creteil-morning_4-half", "Sparse NW\nHalf" + cap_suffix),
+        ("results_creteil-morning_9-full", "Dense NW\nFull" + cap_suffix),
+        ("results_creteil-morning_9-half", "Dense NW\nHalf" + cap_suffix),
+        ("results_creteil-morning_9-quarter", "Dense NW\nQuarter" + cap_suffix),
+    ], "ho", "HO_Total")
+
+    plot_boxplot([
+        ("results_creteil-morning_4-half", "Sparse NW\nHalf" + cap_suffix),
+        ("results_creteil-morning_9-half", "Dense NW\nHalf" + cap_suffix),
+        ("results_creteil-morning_9-quarter", "Dense NW\nQuarter" + cap_suffix),
+    ], "Minimum QoS", "MinQoS", "Minimum QoS per Configuration", percentage=True)
+
+    plot_boxplot_gini([
+        ("results_creteil-morning_4-full", "Sparse NW\nFull" + cap_suffix),
+        ("results_creteil-morning_4-half", "Sparse NW\nHalf" + cap_suffix),
+        ("results_creteil-morning_9-full", "Dense NW\nFull" + cap_suffix),
+        ("results_creteil-morning_9-half", "Dense NW\nHalf" + cap_suffix),
+        ("results_creteil-morning_9-quarter", "Dense NW\nQuarter" + cap_suffix),
+    ], "Load Distribution (Gini Coefficient)", "GiniLoad",
+        "Load Distribution Inequality (Gini Coefficient) per Configuration", percentage=False)
 
 
 if __name__ == "__main__":
